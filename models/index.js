4724be76e5753d9d2acecfdbe6a314c0179e8c29
Dylan Mayfield <dylanmayfield18@gmail.com>
2023-11-16 14:16:01 -0600

    updated require models/index.js and added server.js

--- models/index.js
+++ models/index.js
@@ -1,6 +1,6 @@
 // import models
-const Product = require('./models/Product');
-const Category = require('./models/Category');
+const Product = require('./Product');
+const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
