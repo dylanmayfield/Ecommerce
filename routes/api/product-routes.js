ffc64d0ae40c082036dbcb9ba3063aff28edb1dd
Dylan Mayfield <dylanmayfield18@gmail.com>
2023-11-16 15:35:44 -0600

    fixed bug in product routes, find prodct by id

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -24,7 +24,7 @@ router.get('/:id', async (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
   try {
-    const productData = await Product.findbyPk(req.params.id,{
+    const productData = await Product.findByPk(req.params.id,{
       include: [{ model: Category }, {model: Tag}],
     });
       res.status(200).json(productData)
